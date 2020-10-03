88d1ace3fd97f758dd3c09db5ce25036e0d162bd
Alexandra <aliaxandra@gmail.com>
2020-10-03 14:52:54 +0300

    init: add typical-arrays-problems task and solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+    if (array === undefined || !array.length) {
+		return 0;
+	}
+
+	let min = 0;
+	let minTemp = 0;
+
+	for (let item of array) {
+		minTemp = Math.min(item, minTemp);
+		if (minTemp < min) min = minTemp;
+	}
+
+	return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+    if (array === undefined || !array.length) {
+		return 0;
+	}
+
+	let max = 0;
+	let maxTemp = 0;
+
+	for (let item of array) {
+		maxTemp = Math.max(item, maxTemp);
+		if (maxTemp > max) max = maxTemp;
+	}
+
+	return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+	if (array === undefined|| !array.length) {
+		return 0;
+	}
+
+	let sum = 0;
+	let avg = 0;
+
+	for (let value of array) {
+		sum += value;
+	}
+
+	avg = sum / array.length;
+
+	return avg;
 }
